4dce352f2449af3e2ff82060b49977b2bde408c3
Uchenna Obicheta <uobicheta@hotmail.com>
2024-02-01 19:14:54 -0500

    binary search implementation.

---
+++ Searching/binary_search.js
@@ -0,0 +1,26 @@
+//Binary search is a divide-and-conquer algorithm used to efficiently 
+// locate a target value within a sorted collection (e.g., an array or a list).
+
+//Time: O(log n)
+//Space: O(1)
+
+const binary_search = (array, target) => {
+    let left = 0;
+    let right = array.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (target === array[mid]) {
+            return mid;
+        } else if (target < array[mid]) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return false;
+};
+
+
+console.log(binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9], 4));
