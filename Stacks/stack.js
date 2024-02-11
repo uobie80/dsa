98b5889528d2ce805eca716aa3e9d276b0967117
Uchenna Obicheta <uobicheta@hotmail.com>
2024-02-10 20:38:46 -0500

    Added to_string method and updated comments.

--- Stacks/stack.js
+++ Stacks/stack.js
@@ -2,6 +2,9 @@
 // Useful for tracking a history of steps i.e. browsing history
 // Stack implementation using a linked list
 
+//Time: O(1) -> for push and pop operations
+// Space: O(n)
+
 class Node {
     constructor(val) {
         this.val = val;
@@ -21,27 +24,47 @@ class Stack {
         } else {
             const pushNode = new Node(val);
             pushNode.next = this.top;
+
             this.top = pushNode;
         }
-        this.sixe++;
+        this.size++;
     }
 
     pop() {
         if (this.size === 0) return null;
         const poppedNode = this.top;
         this.top = this.top.next;
+        this.size--;
         return poppedNode.val;
     }
 
     getTop() {
         return this.top.val;
     }
+
+    to_string() {
+        let str = "";
+        let curr = this.top;
+        while (curr !== null) {
+            str += curr.val + ' -> ';
+            curr = curr.next;
+        }
+        console.log(str);
+    }
 }
 
+
+// Test
 const myStack = new Stack();
 myStack.push("1");
 myStack.push("2");
 myStack.push("3");
 myStack.push("4");
 
-console.log(mystack.size());
+console.log(myStack.to_string());
+console.log(myStack.getTop());
+console.log(myStack.size);
+console.log(myStack.pop());
+console.log(myStack.size);
+console.log(myStack.getTop());
+console.log(myStack.to_string());
